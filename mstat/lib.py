5216ee48206292507de555a646089b3a8206c4c3
Eric Zhao <21zhaoe@protonmail.com>
2020-10-12 11:58:56 -0400

    fix: Take file instead of filepath

--- mstat/lib.py
+++ mstat/lib.py
@@ -2,20 +2,20 @@ from __future__ import annotations
 
 import csv
 from typing import List
+from typing import TextIO
 
 from .record import Record
 from .record import User
 
 
-def __read_rows(filename: str, encoding='utf-8') -> List[List[str]]:
-    with open(filename, encoding=encoding) as file:
-        reader = csv.reader(file, delimiter='\t')
-        return [row for row in reader][1:]
+def __read_rows(file: TextIO) -> List[List[str]]:
+    reader = csv.reader(file, delimiter='\t')
+    return [row for row in reader][1:]
 
 
-def read_records(filename: str, encoding='utf-8') -> List[Record]:
-    return [Record.from_row(row) for row in __read_rows(filename, encoding)]
+def read_records(file: TextIO) -> List[Record]:
+    return [Record.from_row(row) for row in __read_rows(file)]
 
 
-def read_users(filename: str, encoding='utf-8') -> List[User]:
-    return User.from_multiple_record_list(read_records(filename, encoding))
+def read_users(file: TextIO) -> List[User]:
+    return User.from_multiple_record_list(read_records(file))
